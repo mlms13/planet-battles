2ca9689ce6e0cfc2038092f98c05776dbeaa8c3b
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-02-15 10:26:30 -0700

    Group file paths together

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,22 +8,35 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
 
     // configuration
-    port = gutil.env.port || 3000;
+    port = gutil.env.port || 3000,
+    paths = {
+      lib: {
+        all: ['./node_modules/phaser/build/phaser.min.js',
+             './node_modules/phaser/build/phaser.map']
+      },
+      js: {
+        main: './src/index.js'
+      },
+      html: {
+        all: ['./src/index.html']
+      },
+      dest: './dist'
+    };
 
 gulp.task('copy', function () {
-  return gulp.src(['./node_modules/phaser/build/phaser.min.js', './src/index.html'])
-    .pipe(gulp.dest('./dist'));
+  return gulp.src(paths.lib.all.concat(paths.html.all))
+    .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('js', function () {
-  return browserify('./src/index.js', {})
+  return browserify(paths.js.main, {})
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('server', function (cb) {
-  var file = new nodeStatic.Server('./dist');
+  var file = new nodeStatic.Server(paths.dest);
 
   require('http').createServer(function (req, res) {
     req.addListener('end', function () {
