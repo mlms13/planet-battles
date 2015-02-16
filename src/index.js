2f7e3c509d98e69ab85dcacea112719801656489
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-02-16 11:40:44 -0700

    Make the planet pretty

--- src/index.js
+++ src/index.js
@@ -3,27 +3,21 @@ var Phaser = window.Phaser;
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   preload: preload,
   create: create,
-  update: update,
-  render: render
+  update: update
 });
-
-var planet;
 
 function preload() {
   game.load.image('space', 'assets/space.png');
+  game.load.image('planet', 'assets/planet.png');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   game.add.sprite(0, 0, 'space');
-  planet = new Phaser.Circle(game.world.centerX, game.world.centerY, 130);
+  game.add.sprite(game.world.centerX - game.cache.getImage('planet').width / 2, game.world.centerY - game.cache.getImage('planet').height / 2, 'planet');
 }
 
 function update() {
   // TODO
-}
-
-function render() {
-  game.debug.geom(planet, '#662211');
 }
