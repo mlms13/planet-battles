79fd3404aa9689a5cfa0538047939976756a0356
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-02-15 21:29:42 -0700

    Add a planet (fixes #1)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ var Phaser = window.Phaser;
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   preload: preload,
   create: create,
-  update: update
+  update: update,
+  render: render
 });
+
+var planet;
 
 function preload() {
   game.load.image('space', 'assets/space.png');
@@ -14,8 +17,13 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   game.add.sprite(0, 0, 'space');
+  planet = new Phaser.Circle(game.world.centerX, game.world.centerY, 130);
 }
 
 function update() {
   // TODO
+}
+
+function render() {
+  game.debug.geom(planet, '#662211');
 }
