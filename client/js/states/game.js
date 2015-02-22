2344ea24cf0c275ff413114e8880dd4d26eaf08e
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-02-21 20:49:01 -0700

    Better code organization

--- client/js/states/game.js
+++ client/js/states/game.js
@@ -1,8 +1,6 @@
 function Game() {}
 
-var space;
-var planet;
-var turrets = [];
+var buildings = {};
 
 Game.prototype = {
   preload: function () {
@@ -12,44 +10,48 @@ Game.prototype = {
     this.load.image('turret', 'assets/turret.png');
   },
 
-  _addTurrets: function (group, howMany, radius, offset) {
-    var sprite, angle, turretX, turretY, i;
+  _createTurrets: function (howMany, radius, offset) {
+    var turrets, sprite, angle, turretX, turretY, i;
+
+    turrets = this.add.group();
 
     for (i = 0; i < howMany; i++) {
       angle = 360 / howMany;
       turretX = Math.cos(i * angle * (Math.PI / 180)) * radius + offset;
       turretY = Math.sin(i * angle * (Math.PI / 180)) * radius + offset;
-      sprite = group.create(turretX, turretY, 'turret');
+      sprite = turrets.create(turretX, turretY, 'turret');
       sprite.anchor.setTo(0.2, 0.5);
-      turrets.push(sprite);
     }
+
+    return turrets;
   },
 
   _createPlanet: function (posX, posY) {
     var planet = this.add.sprite(posX, posY, 'planet');
     var planetRadius = planet.width / 2;
-    var buildings = this.add.group();
-    buildings.colony = buildings.create(planetRadius, planetRadius, 'colony');
+
+    // create colony
+    buildings.colony = this.add.sprite(planetRadius, planetRadius, 'colony');
     buildings.colony.anchor.setTo(0.5);
+    planet.addChild(buildings.colony);
 
-    this._addTurrets(buildings, 3, planetRadius - 10, planetRadius);
+    // creat turrets
+    buildings.turrets = this._createTurrets(3, planetRadius - 10, planetRadius);
+    planet.addChild(buildings.turrets);
 
-    planet.addChild(buildings);
     return planet;
   },
 
   create: function () {
-    var i;
-
     this.physics.startSystem(Phaser.Physics.ARCADE);
 
-    space = this.add.sprite(0, 0, 'space');
-    planet = this._createPlanet(this.world.centerX - 65, this.world.centerY - 65);
+    var space = this.add.sprite(0, 0, 'space');
+    var planet = this._createPlanet(this.world.centerX - 65, this.world.centerY - 65);
     space.addChild(planet);
   },
 
   update: function () {
-    turrets.forEach(function (turret) {
+    buildings.turrets.children.forEach(function (turret) {
       turret.rotation = Phaser.Point.angle(this.input, turret.world);
     }, this);
   }
